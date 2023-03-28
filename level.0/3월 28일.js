d75ea4e3105b13fd7176a8136f2bae5ff026938f
Hyongyi <102146170+Hyongyi@users.noreply.github.com>
2023-03-28 20:41:22 +0900

    3월 28일

--- level.0/3월 28일.js
+++ level.0/3월 28일.js
@@ -3,3 +3,13 @@
 function solution(M, N) {
     return M*N-1
 }
+
+
+////https://school.programmers.co.kr/learn/courses/30/lessons/120886
+
+function solution(before, after) {
+    return before.split("").sort().join("") == after.split("").sort().join("")
+      ? 1
+      : 0;
+  }
+  
