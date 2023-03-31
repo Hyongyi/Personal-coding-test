459ececbe3c957b8e02b81a85ab50b12f1a60ebd
Hyongyi <102146170+Hyongyi@users.noreply.github.com>
2023-03-31 20:59:18 +0900

    Update 3월 31일.js

--- level. 1/3월 31일.js
+++ level. 1/3월 31일.js
@@ -9,3 +9,30 @@ function solution(numbers) {
 function solution(numbers) {
     return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120853
+
+function solution(s) {
+    var x =s.split(" ");
+    var y = 0
+   for(i=0; i<x.length; i++){
+       if(x[i] !== "Z"){
+        y += Number(x[i])
+        } else {
+        y -= Number(x[i-1])
+        }
+   }
+return y
+}
+
+//우수한 풀이
+function solution(s) {
+    const stack = []
+
+    s.split(' ').forEach((target) => {
+        if(target === 'Z') stack.pop();
+        else stack.push(+target)
+    })
+
+    return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+}
