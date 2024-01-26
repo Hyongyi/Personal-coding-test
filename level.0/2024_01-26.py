dd7790ab8569c38b9dd951073c0e6f031bff6d91
Hyoyoung <gyddl123@gmail.com>
2024-01-26 23:07:58 +0900

    24_02_26 코테

---
+++ level.0/2024_01-26.py
@@ -0,0 +1,8 @@
+# 정수가 담긴 리스트 num_list가 주어질 때, 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
+
+from math import prod
+def solution(num_list):
+    if(prod(num_list)> sum(num_list)**2):
+        return 0
+    else :
+        return 1 
