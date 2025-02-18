8e38b93b1b22d538555c1926a4e01d9e5bfe4695
Hyoyoung <gyddl123@gmail.com>
2025-02-18 23:19:57 +0900

    20250218 문제풀이

---
+++ level.0/20250218.js
@@ -0,0 +1,24 @@
+//두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+function solution(a, b, flag) {
+  return flag ? a + b : a - b;
+}
+
+//자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  n = Number(input[0]);
+  n % 2 == 0
+    ? console.log(n.toString() + " is even")
+    : console.log(n.toString() + " is odd");
+});
