cfd60c8fb35adde782e64a35c0a515451b5f4836
Hyoyoung <gyddl123@gmail.com>
2025-02-23 21:31:33 +0900

    20250223 문제풀이

--- 202502223.js
+++ 202502223.js
@@ -23,3 +23,24 @@ function solution(n) {
   if (n % 2 === 1) return ((n + 1) / 2) * ((n + 1) / 2);
   else return (n * (n + 1) * (n + 2)) / 6;
 }
+
+// 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+// 12 ⊕ 3 = 123
+// 3 ⊕ 12 = 312
+// 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+// 단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+
+function solution(a, b) {
+  var aStr = a.toString();
+  var bStr = b.toString();
+
+  var sum1 = Number(aStr + bStr);
+  var sum2 = Number(bStr + aStr);
+
+  return sum1 > sum2 ? sum1 : sum2;
+}
+
+//더 간단한 답
+function solution(a, b) {
+  return Math.max(Number(`${a}${b}`), Number(`${b}${a}`));
+}
