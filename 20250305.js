96ef01124284338577a7dc2c0c4c99f8c53526f9
Hyoyoung <gyddl123@gmail.com>
2025-03-05 23:50:30 +0900

    20250305 문제풀이

--- 20250305.js
+++ 20250305.js
@@ -16,3 +16,11 @@ const solution = (s) => s.toLowerCase().replaceAll("a", "A");
 function solution(strArr) {
   return strArr.map((a, b) => (b % 2 == 0 ? a.toLowerCase() : a.toUpperCase()));
 }
+
+//정수가 담긴 리스트 num_list가 주어질 때, 리스트의 길이가 11 이상이면 리스트에 있는 모든 원소의 합을 10 이하이면 모든 원소의 곱을 return하도록 solution 함수를 완성해주세요.
+
+function solution(num_list) {
+  return num_list.length >= 11
+    ? num_list.reduce((a, b) => a + b)
+    : num_list.reduce((a, b) => a * b);
+}
