229df44b28fc8ac66e7a91cb8ad7b1567715c98d
Hyoyoung <gyddl123@gmail.com>
2025-03-09 23:55:32 +0900

    20250309 문제풀이

--- 20250309.js
+++ 20250309.js
@@ -35,3 +35,9 @@ function solution(n, k) {
 function solution(n_str) {
   return Number(n_str).toString();
 }
+
+//문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+
+function solution(arr) {
+  return arr.join("");
+}
