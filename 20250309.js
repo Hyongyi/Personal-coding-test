38c0d372b898e355e5c66b6804bf4bbd458204cd
Hyoyoung <gyddl123@gmail.com>
2025-03-09 23:53:55 +0900

    20250309 문제풀이

--- 20250309.js
+++ 20250309.js
@@ -8,6 +8,16 @@ function solution(start_num, end_num) {
   return answer;
 }
 
+//정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+function solution(start_num, end_num) {
+  var answer = [];
+  for (var i = start_num; i <= end_num; i++) {
+    answer.push(i);
+  }
+  return answer;
+}
+
 //정수 n과 k가 주어졌을 때, 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
 
 function solution(n, k) {
